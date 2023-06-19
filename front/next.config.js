bcd9db02e32f9959561a70bb53c00c69d5522aa6
kaneda_masaya <kaneda-masaya@g.ecc.u-tokyo.ac.jp>
2023-06-20 03:13:19 +0900

    next.config

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/init',
+        destination: `${process.env.SERVER_URL}/init`,
+      },
+      {
+        source: '/graph',
+        destination: `${process.env.SERVER_URL}/test_graph`,
+      },
+      {
+        source: '/:hash*',
+        destination: `${process.env.SERVER_URL}/:hash*`,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
