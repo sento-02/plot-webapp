214cb952d1727952aa764afff9bb41b0895a5734
kaneda_masaya <kaneda-masaya@g.ecc.u-tokyo.ac.jp>
2023-06-20 10:33:52 +0900

    MUI 01

---
+++ front/app/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // この色を変更して任意の色を設定します
+    },
+    secondary: {
+      main: '#dc004e', // この色を変更して任意の色を設定します
+    },
+  },
+});
+
+export default theme;
